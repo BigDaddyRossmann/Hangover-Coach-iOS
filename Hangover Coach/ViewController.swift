7477cf1bdfb95693f7472db8b90377b4ece4446e
David Chavez <david@psychosity.net>
2015-04-27 01:21:45 -0700

    Initial commit

---
+++ Hangover Coach/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hangover Coach
+//
+//  Created by David Chavez on 4/27/15.
+//  Copyright (c) 2015 Big Daddy Rossmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
